d770fd02cab796671a77ee65b063bb39fc2137c5
joernweissenborn <joern.weissenborn@gmail.com>
2016-04-28 21:42:10 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
 from setuptools import setup, Extension
 import os
 
-swigfile = os.environ['GOPATH']+'/src/github.com/joernweissenborn/thingiverseio/shared_library/libthingiverse.i'
 
-include = os.environ['GOPATH']+'/src/github.com/joernweissenborn/thingiverseio/shared_library/include/'
-
-_libthingiverseio = Extension('_libthingiverseio',
-                           sources=[swigfile],
-                           swig_opts=['-I'+include]
-                           )
 
 
 setup(name='pythingiverseio',
@@ -22,7 +15,6 @@ setup(name='pythingiverseio',
       install_requires=[
         'msgpack-python',
       ],
-      ext_modules = [_libthingiverseio],
       test_suite='nose.collector',
       tests_require=['nose'],
 )
